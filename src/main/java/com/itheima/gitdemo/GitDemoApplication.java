900dc18f2625a4fbcf92d4b3ee02a8bed3c40ef8
Rain <610473966@qq.com>
2025-04-06 15:27:45 +0800

    four commit

--- src/main/java/com/itheima/gitdemo/GitDemoApplication.java
+++ src/main/java/com/itheima/gitdemo/GitDemoApplication.java
@@ -9,7 +9,8 @@ public class GitDemoApplication {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello github");
-
+        System.out.println("third");
+        System.out.println("four");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
