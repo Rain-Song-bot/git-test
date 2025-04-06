78b15ba150531fab7c9ae34273687660e9294218
Rain <610473966@qq.com>
2025-04-06 15:15:13 +0800

    first commit

---
+++ src/main/java/com/itheima/gitdemo/GitDemoApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+
+}
